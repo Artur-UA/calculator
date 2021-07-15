4a276f92bf69eb34f7832fd5a1c6e8b484cca6e5
Artur <63858580+Artur-UA@users.noreply.github.com>
2021-07-15 23:52:52 +0300

    Add files via upload

--- calc.js
+++ calc.js
@@ -11,17 +11,27 @@ $('#material select').change(function () {
 
 function calcTot() {
 
-    const result = document.querySelector('.all_price');
-    const material = document.querySelector('.select_sel:not([style*="display: none;"])').value;
-    const treatment = document.querySelector('.treatment').value;
+    let result = document.querySelector('.all_price');
+    let massa = document.querySelector('.vv');
+    let material = document.querySelector('.select_sel:not([style*="display: none;"])').value;
+    let treatment = document.querySelector('.treatment').value;
 
-    const thickness = document.querySelector('.select_sel:not([style*="display: none;"])').data-ves;
+    let thickness = document.querySelector('.select_sel:not([style*="display: none;"])').options[document.querySelector('.select_sel:not([style*="display: none;"]').selectedIndex].text;
+    let thickness_mm = thickness.replace(/[^+\d]/g, '')
+
 
     let sum;
     let width = parseFloat($("#width_par").val());
     let height = parseFloat($("#height_par").val());
     let area = width * height / 1000000;
-    let facet_m = (width + height) * 2 / 1000
+    let facet_m = (width + height) * 2 / 1000;
+    let ves = (width * height) / 10000 * 2.5 * thickness_mm / 100;
+    massa.textContent = ves;
+
+
+    let sum1 = area * material;
+    result.textContent = sum1;
+
 
     console.log(width);
     console.log(height);
@@ -30,17 +40,80 @@ function calcTot() {
     console.log(material);
     console.log(treatment);
     console.log(thickness);
+    console.log(thickness_mm);
 
-/*     let treatment_price
-    if (treatment == "0") {
+    let treatment_price = 0;
 
-        console.log("0000")
-    } */
+    if (treatment == "1") {
 
-
-    if (!sum) {
-        result.textContent = "Нехватает данных";
-        return;
+        switch (thickness_mm) {
+            case "4":
+                treatment_price = 100;
+                break;
+            case "5":
+                treatment_price = 112;
+                break;
+            case "6":
+                treatment_price = 125;
+                break;
+            case "8":
+                treatment_price = 150;
+                break;
+            case "10":
+                treatment_price = 163;
+                break;
+            case "12":
+                treatment_price = 338;
+                break;
+            case "15":
+                treatment_price = 432;
+                break;
+            case "19":
+                treatment_price = 557;
+                break;
+        }
     }
+    if (treatment == "2") {
+
+        switch (thickness_mm) {
+            case "4":
+                treatment_price = 57;
+                break;
+            case "5":
+                treatment_price = 63;
+                break;
+            case "6":
+                treatment_price = 75;
+                break;
+            case "8":
+                treatment_price = 100;
+                break;
+            case "10":
+                treatment_price = 119;
+                break;
+            case "12":
+                treatment_price = 232;
+                break;
+            case "15":
+                treatment_price = 257;
+                break;
+            case "19":
+                treatment_price = 394;
+                break;
+        }
+    }
+
+
+    /* treatment_price = 5;
+    console.log("0000"); */
+
+
+    console.log(treatment_price);
+
+
+    /*  if (!sum) {
+         result.textContent = "Нехватает данных";
+         return;
+     } */
 }
 /* calcTot() */
