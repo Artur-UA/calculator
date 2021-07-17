1d2dda6e01c20bd775844c08f9d63a81d627d96c
Artur <63858580+Artur-UA@users.noreply.github.com>
2021-07-17 13:52:46 +0300

    Add files via upload

--- calc.js
+++ calc.js
@@ -9,6 +9,7 @@ $('#material select').change(function () {
     cxxc = '.' + cxxc + ' option:selected';
 });
 
+
 function calcTot() {
 
     let result = document.querySelector('.all_price');
@@ -28,9 +29,6 @@ function calcTot() {
     let ves = (width * height) / 10000 * 2.5 * thickness_mm / 100;
     massa.textContent = ves;
 
-
-    let sum1 = area * material;
-    result.textContent = sum1;
 
 
     console.log(width);
@@ -102,14 +100,275 @@ function calcTot() {
                 break;
         }
     }
-
-
-    /* treatment_price = 5;
-    console.log("0000"); */
-
-
     console.log(treatment_price);
 
+    /*     ОБРАБОТКА  */
+
+    let obrabotka_price = 0;
+    let obrabotka = document.querySelector('.treatment').value;
+
+    if (obrabotka == 1) {
+        switch (thickness_mm) {
+            case "4":
+                obrabotka_price = 100 * facet_m;
+                break;
+            case "5":
+                obrabotka_price = 112 * facet_m;
+                break;
+            case "6":
+                obrabotka_price = 125 * facet_m;
+                break;
+            case "8":
+                obrabotka_price = 150 * facet_m;
+                break;
+            case "10":
+                obrabotka_price = 163 * facet_m;
+                break;
+            case "12":
+                obrabotka_price = 338 * facet_m;
+                break;
+            case "15":
+                obrabotka_price = 432 * facet_m;
+                break;
+            case "19":
+                obrabotka_price = 557 * facet_m;
+                break;
+        }
+    }
+
+    if (obrabotka == 2) {
+        switch (thickness_mm) {
+            case "4":
+                obrabotka_price = 57 * facet_m;
+                break;
+            case "5":
+                obrabotka_price = 63 * facet_m;
+                break;
+            case "6":
+                obrabotka_price = 75 * facet_m;
+                break;
+            case "8":
+                obrabotka_price = 100 * facet_m;
+                break;
+            case "10":
+                obrabotka_price = 119 * facet_m;
+                break;
+            case "12":
+                obrabotka_price = 232 * facet_m;
+                break;
+            case "15":
+                obrabotka_price = 257 * facet_m;
+                break;
+            case "19":
+                obrabotka_price = 394 * facet_m;
+                break;
+        }
+    }
+
+    console.log(`Стоимость обработки ${obrabotka_price}`);
+
+    /*     НЕ ЗАКОНЧЕНО ДЛЯ ФИГУРЫ */
+
+    /*     ОБРАБОТКА КОНЕЦ */
+
+
+
+
+    /*     фацет */
+    let facet_value = document.querySelector('.sell1').value;
+    console.log(facet_value);
+
+    let facet_price = facet_value * facet_m;
+    console.log(`Стоимость фацета ${facet_price}`);
+
+
+    /*     НЕ ЗАКОНЧЕНО ДЛЯ ФИГУРЫ */
+
+
+    /*     фацет конец*/
+    /*     отверстия */
+
+    let otverst_mm = document.querySelector('#otverst').value;
+    let otverst_amount = document.querySelector('.prs').value;
+    console.log(`диаметр отверстия ${otverst_mm}`);
+    console.log(`количество отверстий ${otverst_amount}`);
+
+    let otverst_price = 0;
+    if (thickness_mm >= 4 && thickness_mm <= 6) {
+        console.log("диаметр 1");
+        if (otverst_mm >= 3 && otverst_mm <= 12) {
+            otverst_price = 88;
+        }
+        if (otverst_mm >= 13 && otverst_mm <= 30) {
+            otverst_price = 100;
+        }
+        if (otverst_mm >= 31 && otverst_mm <= 70) {
+            otverst_price = 138;
+        }
+        if (otverst_mm >= 71 && otverst_mm <= 90) {
+            otverst_price = 625;
+        }
+    }
+    if (thickness_mm >= 8 && thickness_mm <= 13) {
+        console.log("диаметр 2");
+        if (otverst_mm >= 3 && otverst_mm <= 12) {
+            otverst_price = 100;
+        }
+        if (otverst_mm >= 13 && otverst_mm <= 30) {
+            otverst_price = 119;
+        }
+        if (otverst_mm >= 31 && otverst_mm <= 70) {
+            otverst_price = 163;
+        }
+        if (otverst_mm >= 71 && otverst_mm <= 90) {
+            otverst_price = 625;
+        }
+    }
+    if (thickness_mm >= 15 && thickness_mm <= 19) {
+        console.log("диаметр 3");
+        if (otverst_mm >= 3 && otverst_mm <= 12) {
+            otverst_price = 119;
+        }
+        if (otverst_mm >= 13 && otverst_mm <= 30) {
+            otverst_price = 138;
+        }
+        if (otverst_mm >= 31 && otverst_mm <= 70) {
+            otverst_price = 200;
+        }
+        if (otverst_mm >= 71 && otverst_mm <= 90) {
+            otverst_price = 625;
+        }
+    }
+
+    console.log(`Стоимость отверстия ${otverst_price}`);
+    otverst_price = otverst_price * otverst_amount;
+    console.log(`Стоимость за все отверстия ${otverst_price}`);
+
+    /*     отверстия конец*/
+
+    /*     вырезы */
+    let vyrez = document.querySelector('.vnu').value;
+    let cena_vyreza = 0;
+    switch (thickness_mm) {
+        case "4":
+            cena_vyreza = 438 * vyrez;
+            break;
+        case "5":
+            cena_vyreza = 563 * vyrez;
+            break;
+        case "6":
+            cena_vyreza = 563 * vyrez;
+            break;
+        case "8":
+            cena_vyreza = 625 * vyrez;
+            break;
+        case "10":
+            cena_vyreza = 688 * vyrez;
+            break;
+        case "12":
+            cena_vyreza = 963 * vyrez;
+            break;
+        case "15":
+            cena_vyreza = 1250 * vyrez;
+            break;
+        case "19":
+            cena_vyreza = 1875 * vyrez;
+            break;
+    }
+    console.log(vyrez);
+    console.log(`Цена за все вырезы ${cena_vyreza}`);
+    /*     вырезы конец*/
+
+    /*     закалка */
+    $('#material select').on('change', function () {
+        var s = $(this).find('option:selected').text() == "Стекло";
+        if (s) {
+            console.log("ffff")
+            $('.zakalka input').removeAttr('disabled');
+            $('.zakalka .g_tit').css('color', 'black');
+            $('.zakalka').removeClass('dis');
+        } else {
+            console.log("zzzz")
+            $('.zakalka input').removeAttr('checked')
+            $('.zakalka input').attr('disabled', 'disabled');
+            $('.zakalka .g_tit').css('color', 'gray');
+            $('.zakalka').addClass('dis');
+        }
+    });
+
+    let zakalka = 0;
+    let zaka = $('#check222').is(':checked')
+    if (zaka) {
+        switch (thickness_mm) {
+            case "4":
+                console.log("zakalka")
+                zakalka = 713 * area;
+                break;
+            case "5":
+                zakalka = 713 * area;
+                break;
+            case "6":
+                zakalka = 825 * area;
+                break;
+            case "8":
+                zakalka = 925 * area;
+                break;
+            case "10":
+                zakalka = 1038 * area;
+                break;
+            case "12":
+                zakalka = 1513 * area;
+                break;
+            case "15":
+                zakalka = 2963 * area;
+                break;
+            case "19":
+                zakalka = 3125 * area;
+                break;
+        }
+    }
+
+    console.log(`Закалка ${zakalka} руб`)
+
+    /*     закалка конец*/
+    /*     расстояние */
+    $('.deliv_type').on('change', function () {
+        var t = $('.deliv_type select option:selected').val();
+        if (t == 3) {
+            $('.deliverys2').css('opacity', 1);
+        } else {
+            $('.deliverys2').css('opacity', 0);
+            $('.deliverys2 input').val(0);
+        }
+    });
+
+    let distance = document.querySelector('.distance').value;
+    let distance_mkad = document.querySelector('.in5 input').value;
+    let distance_price = +0;
+    switch (distance) {
+        case "1":
+            distance_price = 2500;
+            break;
+        case "2":
+            distance_price = 3000;
+            break;
+        case "3":
+            distance_price = 2500 + Number(distance_mkad * 35);
+            break;
+        case "8":
+            distance_price = 0;
+            break;
+    }
+    console.log(distance);
+    console.log(distance_mkad);
+    console.log(distance_price);
+    /*     расстояние конец */
+
+
+
+    let sum1 = area * material + distance_price + cena_vyreza + zakalka + otverst_price;
+    result.textContent = sum1;
+    console.log(`Итоговая стоимость ${sum1} рублей`);
 
     /*  if (!sum) {
          result.textContent = "Нехватает данных";
